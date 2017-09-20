21d46462d144eb8d421857c4118baf87afff5961
Monostra <senyukmykhailo@gmail.com>
2017-09-20 17:18:34 +0300

    Commit

---
+++ src/repeat/classes_project/java_8/Drums.java
@@ -0,0 +1,15 @@
+package repeat.classes_project.java_8;
+
+/**
+ * Created by Mykhailo on 20/09/2017.
+ */
+public class Drums extends Instrument{
+
+    public Drums(int weight) {
+        super(weight);
+    }
+
+    public void play() {
+        System.out.println("drums play");
+    }
+}
