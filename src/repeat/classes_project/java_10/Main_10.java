f75dba8c223da2a9c3883c52e578c39580b6a099
Monostra <senyukmykhailo@gmail.com>
2017-09-20 19:07:09 +0300

    Commit

---
+++ src/repeat/classes_project/java_10/Main_10.java
@@ -0,0 +1,24 @@
+package repeat.classes_project.java_10;
+
+/**
+ * Created by Mykhailo on 20/09/2017.
+ */
+public class Main_10 {
+    static {
+        System.out.println("main static block");
+    }
+
+    {
+        System.out.println("main simble block");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("main method");
+        Main_10 main10 = new Main_10();
+        Person person = new Person("vova", 23);
+    }
+
+    static {
+        System.out.println("main statick block 2");
+    }
+}
---
+++ src/repeat/classes_project/java_10/Person.java
@@ -0,0 +1,47 @@
+package repeat.classes_project.java_10;
+
+/**
+ * Created by Mykhailo on 20/09/2017.
+ */
+public class Person {
+
+    static {
+        System.out.println("person statick block");
+    }
+
+    {
+        System.out.println("person simple block");
+    }
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
